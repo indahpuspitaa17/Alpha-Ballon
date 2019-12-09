b612aefff4667353fb5e516f2b136798ca02418c
Indah Puspita <indah_indahpuspita@apps.ipb.ac.id>
2019-12-09 12:18:37 +0700

    initial commit

---
+++ Assets/script/Loading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+
+	public Transform masukanLoadingbar;
+
+	[SerializeField]
+	private float nilaiSekarang;
+	[SerializeField]
+	private float nilaiKecepatan;
+
+	void Update () {
+		if (nilaiSekarang < 100) {
+			nilaiSekarang += nilaiKecepatan * Time.deltaTime;
+			Debug.Log ((int)nilaiSekarang);
+
+		} else {
+			Application.LoadLevel ("START");
+		}
+		masukanLoadingbar.GetComponent<Image> ().fillAmount = nilaiSekarang / 100;
+	}
+
+}
---
+++ Assets/script/Menu_Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour {
+
+	public GameObject MenuPanel;
+	public GameObject InfoPanel;
+
+	void Start () {
+		MenuPanel.SetActive (true);
+		InfoPanel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+public void MainClicked()
+{
+	Application.LoadLevel("pecahkan balon");
+}
+public void InfoClicked()
+{
+	MenuPanel.SetActive(false);
+	InfoPanel.SetActive (true);
+}
+public void Quit_clicked()
+{
+	Application.Quit();
+}
+	public void KembaliClicked()
+	{
+		MenuPanel.SetActive (true);
+		InfoPanel.SetActive (false);
+	}
+}
